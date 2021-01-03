9f44284713e14a26e0ec5260219ea80a59bebc34
HessaAlRahmani <76619150+HessaAlRahmani@users.noreply.github.com>
2021-01-03 21:22:23 +0300

    Q1+Q2+Q3(with the bonus)

--- question1/question1.js
+++ question1/question1.js
@@ -1,1 +1,21 @@
 // 👇🏻 قم بحل السؤال الأول هنا
+let urName= "حصة"
+function sayHello(name){
+    
+    
+
+}
+
+function sayHello(name){
+    if(name == null){
+        console.log('مرحبا , بدون اسم ')   
+    }
+    else if (name != null){
+        console.log( 'مرحبا ,  %s', name);
+    }
+
+}
+
+sayHello(urName);
+
+
